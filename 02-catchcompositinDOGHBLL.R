702eb624774d80b841af78c2319e5f30ba46520e
Davidson <Lindsay.Davidson@dfo-mpo.gc.ca>
2023-07-20 09:18:31 -0700

    catch split by length

--- 02-catchcompositinDOGHBLL.R
+++ 02-catchcompositinDOGHBLL.R
@@ -1,5 +1,4 @@
-#Length and catch composition differences between the DOG and HBLL surveys
-
+# Length and catch composition differences between the DOG and HBLL surveys
 
 # library -----------------------------------------------------------------
 library(gfdata)
@@ -32,16 +31,26 @@ samps <- readRDS("output/dogfish_samps.rds")
 # filter data  -----------------------------------------------------------
 samps <- samps |> mutate(name = ifelse(year %in% c(1986, 1989), "DOGJhooks", survey_abbrev))
 samps <- samps %>%
-  mutate(dmy = lubridate::ymd(trip_start_date)) |> 
-  mutate(julian = lubridate::yday(dmy)) 
-ggplot(samps, aes(year, julian, group = name, colour = name)) + geom_point() + geom_line() +theme_classic()
+  mutate(dmy = lubridate::ymd(trip_start_date)) |>
+  mutate(julian = lubridate::yday(dmy))
 
+ggplot(samps, aes(year, julian, group = name, colour = name)) +
+  geom_point() +
+  geom_line() +
+  theme_classic()
+
+samps %>%
+  filter(survey_abbrev == "HBLL INS N") %>%
+  mutate(max = max(julian)) |> 
+  select(year, julian) |> distinct()
+
+# id date
 samps <- filter(samps, sex %in% c(1, 2))
-#samps <- filter(samps, year >= 2000)
+# samps <- filter(samps, year >= 2000)
 range(samps$length)
-samps |> 
-  group_by(year) |> 
-  filter(length < 25) |> 
+samps |>
+  group_by(year) |>
+  filter(length < 25) |>
   tally()
 
 samps <- filter(samps, length > 25)
@@ -52,31 +61,46 @@ sort(unique(samps$sex)) # 1 male, 2 female, 3 unknown?
 ggplot() +
   geom_density(
     data = samps, aes(length,
-                      group = as.factor(name),
-                      fill = as.factor(name)
+      group = as.factor(name),
+      fill = as.factor(name)
     ),
     alpha = 0.35, size = 1, colour = "black"
   ) +
-  facet_grid(~sex) +
+  facet_wrap(~sex) +
   theme_classic() +
   geom_rug(data = samps, aes(length)) +
   scale_fill_manual(values = c("lightblue", "darkblue", "yellow", "orange")) +
   # scale_fill_viridis_d() +
   ylab(label = "Density") +
-  xlab(label = "Length")
+  xlab(label = "Length") 
+
+ggplot() +
+  geom_density(
+    data = filter(samps, name != "DOGJhooks"), aes(length,
+                      group = as.factor(name),
+                      fill = as.factor(name)
+    ),
+    alpha = 0.35, size = 1, colour = "black"
+  ) +
+  facet_wrap(~sex) +
+  theme_classic() +
+  scale_fill_manual(values = c("lightblue", "yellow", "orange")) +
+  # scale_fill_viridis_d() +
+  ylab(label = "Density") +
+  xlab(label = "Length") 
 
 ggplot() +
   geom_density(
     data = filter(samps, name %in% c("DOG", "HBLL INS S")), aes(length,
-                      group = as.factor(name),
-                      fill = as.factor(name)
+      group = as.factor(name),
+      fill = as.factor(name)
     ),
     alpha = 0.35, size = 1, colour = "black"
   ) +
   facet_grid(~sex) +
   theme_classic() +
   geom_rug(data = samps, aes(length)) +
-  #scale_fill_manual(values = c("lightblue", "darkblue", "yellow", "orange")) +
+  # scale_fill_manual(values = c("lightblue", "darkblue", "yellow", "orange")) +
   scale_fill_viridis_d() +
   ylab(label = "Density") +
   xlab(label = "Length")
@@ -111,7 +135,7 @@ ggplot() +
   #            colour = as.factor(survey_series_desc), alpha = 0.15)) +
   geom_boxplot(
     data = filter(samps, sex %in% c(1, 2)), aes(as.factor(year), length,
-                                                fill = as.factor(name)
+      fill = as.factor(name)
     ),
     alpha = 0.35, size = 1, colour = "black"
   ) +
@@ -120,11 +144,30 @@ ggplot() +
   theme_classic() +
   scale_x_discrete(breaks = c(2003, 2009, 2015, 2022)) +
   # geom_rug(data = samps, aes(length)) +
-  #scale_fill_manual(values = c("blue", "darkblue", "yellow")) +
+  # scale_fill_manual(values = c("blue", "darkblue", "yellow")) +
   # scale_fill_viridis_d() +
   ylab(label = "Length") +
   xlab(label = "Year")
 
+x <- filter(samps, name %in% c("HBLL INS S", "DOG"))
+ggplot() +
+  # geom_jitter(data = samps, aes(year, length,
+  #            colour = as.factor(survey_series_desc), alpha = 0.15)) +
+  geom_boxplot(
+    data = filter(x, sex %in% c(1, 2)), aes(as.factor(year), length,
+                                                fill = as.factor(name)
+    ),
+    alpha = 0.35, size = 1, colour = "black"
+  ) +
+  # facet_wrap(~survey_series_desc)
+  facet_grid(~ sex) +
+  theme_classic() +
+  scale_x_discrete(breaks = c(2003, 2009, 2015, 2022)) +
+  # geom_rug(data = samps, aes(length)) +
+  # scale_fill_manual(values = c("blue", "darkblue", "yellow")) +
+  # scale_fill_viridis_d() +
+  ylab(label = "Length") +
+  xlab(label = "Year")
 
 
 # anova - are the group means different? ----------------------------------
@@ -135,14 +178,14 @@ mean(f$length[(f$name) == "HBLL INS S"])
 
 aov <- aov(length ~ name, data = filter(samps, sex == 2))
 summary(aov)
-#plot(aov)
-tukey<-TukeyHSD(aov)
+# plot(aov)
+tukey <- TukeyHSD(aov)
 tukey
 
 aov <- aov(length ~ name, data = filter(samps, sex == 1))
 summary(aov)
-#plot(aov)
-tukey<-TukeyHSD(aov)
+# plot(aov)
+tukey <- TukeyHSD(aov)
 tukey
 plot(tukey)
 
@@ -152,17 +195,57 @@ plot(tukey)
 f <- filter(samps, sex == 2 & name %in% c("DOG", "HBLL INS S"))
 f <- filter(samps, sex == 2 & name == "DOG")
 f <- filter(samps, sex == 2 & name == "HBLL INS S")
-m <- glm (length ~ name*year, data = f, family = gaussian())
+m <- glm(length ~ name * year, data = f, family = gaussian())
 summary(m)
 
 # Ratio of M:F  -----------------------------------------------------------
 
-#females maturing 77 - 95.5
-#males maturing 
+# females maturing 77 - 95.5
+# males maturing
 samps
 glimpse(samps)
 
+samps |> drop_na(weight) |> tally()
+samps |> tally()
+115952 - 6984 #lots of NAs for weight
+
+#use maturity DFO of 55 
+#these legnths are from a length weight and length matuirty curve calculated in 
+#split-index_by_regionandmaturity.R
+test <- samps |>
+  filter(sex %in% c(1, 2)) |>
+  mutate(lengthgroup = ifelse(length >= 77 & length < 95.5 & sex == 2, "maturingf",
+                              ifelse(length >= 65.1 & length < 76.7 & sex == 1 , "maturingm",
+                                     ifelse(length >= 95.5 & sex == 2, "mf",
+                                            ifelse(length >= 76.7 & sex == 1, "mm",
+                                                   ifelse(length < 77 & sex == 2, "imm",
+                                                          ifelse(length < 65.1 & sex == 1, "imm",
+                                                                 NA))))))) |>
+  group_by(year, lengthgroup, name) |> 
+  mutate(count =  n())
+  
+ggplot(filter(test, name != "DOGJhooks"), aes(year, count, group = name, colour = name)) +
+  geom_line() +
+  facet_wrap(~lengthgroup, scales = "free") + theme_classic() + geom_point() 
+  
 
 # Mature female ratio -----------------------------------------------------
 
+test2 <- test |> 
+  group_by(year, fishing_event_id, name) |> 
+  filter(lengthgroup != "immatures") |> 
+  mutate(sumall =  n()) 
+test3 <- test |> 
+  group_by(year, name) |> 
+  filter(lengthgroup  == "mf") |> 
+  summarize(summf = n()) 
+testmm <- test |> 
+  group_by(year, name) |> 
+  filter(lengthgroup  == "mm") |> 
+  summarize(summm = n()) 
 
+final <- inner_join(test3, testmm, by = c("year", "name"))
+final <- final |> 
+  mutate(ratio = summf/summm*100)
+ggplot(final, aes(year, ratio, group = name)) + geom_point() + geom_line() + facet_wrap(~name) + 
+  ylab("Ratio (Mature females:Mature Males)")
